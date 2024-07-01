d5b66822a33421ec12a779ac9a125a1196b88546
momoDroid <git.judevl@gmail.com>
2024-07-01 13:36:18 +0800

    Update admin.Logout.php

--- MMOCODEX/admin-panel/admin-auth/admin.Logout.php
+++ MMOCODEX/admin-panel/admin-auth/admin.Logout.php
@@ -4,5 +4,5 @@
             session_start();
             session_unset();
             session_destroy();
-            header("location:http://localhost/MMOCODEX_TEST/admin-panel/admin-auth/admin.Login.php");
+            header("location:http://localhost/MMOCODEX/admin-panel/admin-auth/admin.Login.php");
 ?>
